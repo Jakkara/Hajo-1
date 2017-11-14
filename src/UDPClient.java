9a389a1f6ff871685478cd14a67163877965e9e1
jakarai <jakarai@utu.fi>
2017-11-14 12:24:13 +0200

    It works ehkä

--- src/UDPClient.java
+++ src/UDPClient.java
@@ -1,5 +1,40 @@
+import javax.swing.*;
+import javax.xml.crypto.Data;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.io.*;
+import java.net.*;
+
 public class UDPClient {
-    public static void main(String[] args) {
-        System.out.print("juu moi matafaka");
+    private InetAddress activeIPAddress;
+    private int activePortNumber = 3126;
+    private DatagramSocket socket;
+
+
+    public UDPClient() {
+    }
+
+    public void createSocket() {
+        try {
+            socket = new DatagramSocket();
+            activeIPAddress = InetAddress.getByName("localhost");
+        } catch (SocketException sE) {
+            sE.printStackTrace();
+        } catch (UnknownHostException uhE) {
+            uhE.printStackTrace();
+        }
+    }
+
+    public void sendPacket(String message) {
+        try {
+            byte[] dataArrayToSend = message.getBytes();
+            DatagramPacket packetToSend = new DatagramPacket(dataArrayToSend, dataArrayToSend.length, activeIPAddress, activePortNumber);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
     }
 }
+
+
